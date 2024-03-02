872bd149280b89cb29356230cdcb72e42e7e0b24
Leonardo Neri <leonardoneri01@outlook.com>
2024-03-02 18:21:14 -0300

    fix: checkOperations

--- public/main.js
+++ public/main.js
@@ -1,4 +1,14 @@
-import { date } from '../assets/service/date.js';  
+import { date } from '../assets/service/date.js';
+
+const DAYS = {
+    SUNDAY: 'Sunday',
+    MONDAY: 'Monday',
+    TUESDAY: 'Tuesday',
+    WEDNESDAY: 'Wednesday',
+    THURSDAY: 'Thursday',
+    FRIDAY: 'Friday',
+    SATURDAY: 'Saturday',
+}
 
 const checkOperation = async () => {
     const { dayOfWeek, hours } = date();
@@ -6,14 +16,26 @@ const checkOperation = async () => {
     const endHoursWeekday = 22;
     const startHourWeekend = 17;
     const endHoursWeekend = 22;
+    const openingHours = document.getElementById('opening-hours');
 
-    if ((dayOfWeek == 'Saturday' || dayOfWeek == 'Sunday') && hours >= startHourWeekend && hours <= endHoursWeekend) {
-        const openingHours = document.getElementById('opening-hours');
-        openingHours.style.backgroundColor = 'green';
-    } else if (hours >= startHourWeekday && hours <= endHoursWeekday){
-        const openingHours = document.getElementById('opening-hours');
-        openingHours.style.backgroundColor = 'green';
+    switch (dayOfWeek) {
+        case DAYS.SATURDAY:
+        case DAYS.SUNDAY:
+            if (hours >= startHourWeekend && hours <= endHoursWeekend) {
+                openingHours.style.backgroundColor = 'green'
+            }
+            break
+        case DAYS.MONDAY:
+        case DAYS.TUESDAY:
+        case DAYS.WEDNESDAY:
+        case DAYS.THURSDAY:
+        case DAYS.FRIDAY:
+            if (hours >= startHourWeekday && hours <= endHoursWeekday) {
+                openingHours.style.backgroundColor = 'blue'
+            }
+            break
     }
 };
+
 
 checkOperation();
